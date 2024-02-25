be8602514b7c6d68fbad94fc0e4b473c223224e4
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-02-25 08:29:23 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(null>0); // false 0!>0
+console.log(null==0); //  
+console.log(null>=0); // true 0>=0 
+
+console.log(undefined == 0);
+
+// === => strict check
+
+console.log("2" === 2);
