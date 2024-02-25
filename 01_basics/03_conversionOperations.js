896f12eddadf498ae81da8ef2b6fb2d24da3d8d5
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-02-25 06:07:37 +0000

    conversion on Operators

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,45 @@
+// let score = 33
+
+// console.log(typeof score) // number
+// console.log(typeof (score)) // number
+
+// let score1 = "33abc"
+
+// console.log(typeof score1) // string
+// console.log(typeof (score1)) // string
+
+// let valueInNumber = Number(score1)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let isLoggedIn = "123"
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true // 0 => false
+// "" => false
+// "Sahil" => true
+
+// let sumNumber = 33
+// let stringNumber = String(sumNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************** Operations ************************* 
+
+// let str1 = "Sahil"
+// let str2 = " Sharma"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+
+
+
