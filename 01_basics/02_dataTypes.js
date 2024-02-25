b8152be5d273df5d6f03768225efca78e64b3957
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-02-25 04:21:33 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3);
+console.log("Sahil");
+
+let name = "Sahil"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number = 2 to power 53
+// bigint
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
+
+
+
+
