c752df22161b9af97c0c58bfd3cd9609b535b01b
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-02-28 05:27:23 +0000

    date and time

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,28 @@
+// dates
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString()); // Wed Feb 28 2024
+// console.log(myDate.toLocaleString()); // 2/28/24, 4:22:17 AM
+// console.log(typeof (myDate)) // object
+
+let myCreatedDate = new Date("02-28-2024")
+// console.log(myCreatedDate.toLocaleString())
+// console.log(myCreatedDate.toDateString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDay())
+
+newDate.toLocaleString('default', {
+    
+})
+
+
+
+
