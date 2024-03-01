01630ac0dabaf1da89bccce928fdd67dd79317b3
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-03-01 17:34:24 +0000

    arrow functions in js

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,54 @@
+// const user = {
+//     username: "Sahil",
+//     price: 999,
+
+//     welcomeMessage : function (){
+//         console.log(`${this.username}, welcome to the website`);
+//         console.log(this);
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+// function chai (){
+//     let username = "Sahil"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "Sahil"
+//     console.log(this.username);
+// }
+
+
+// const chai = () => {
+//     let username = "Sahil"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2; // explicit return
+// }
+
+// console.log(addTwo(3,7))
+
+
+// const addTwo = (num1, num2) =>  num1 + num2; // implicit return 
+// const addTwo = (num1, num2) =>  (num1 + num2); // implicit return 
+
+const addTwo = (num1, num2) =>  ({username: "Sahil"}) 
+
+console.log(addTwo(3,7))
+
+
+// const myArray = [2,3,4,5,6]
+
+// myArray.forEach(() => {})
