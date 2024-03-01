5bb9fa9a3e08cd02e5303e17a3a6410b9e0412c5
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-03-01 09:27:55 +0000

    scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,61 @@ let a = 300
 if(true){ // block scope inside if block
     let a = 10
     const b = 20
-    console.log(`Inner : ${a}`)
+    // console.log(`Inner : ${a}`)
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one (){
+    const username = " Sahil"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website); // scope finished here
+
+    two()
+}
+// one()
+
+if (true) {
+    const username = "Sahil"
+    if(username === "Sahil"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+} 
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++
+  
+addOne(4)
+
+function addOne(num){
+    return num+1
+}
+
+const addTwo = function(num){
+    return num+2
+}
+
+addTwo(5)
+
+
+
+
+
+
+
+
+
+
+
+
