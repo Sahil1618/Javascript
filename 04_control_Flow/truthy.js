a7f713ab2002aa851992b62ca75e2db928778807
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-03-02 17:00:20 +0000

    truthy and falsy values in js

---
+++ 04_control_Flow/truthy.js
@@ -0,0 +1,13 @@
+const useremail = [] // true
+
+if(useremail){
+    console.log("got user email");
+} else console.log("Dont have user email");
+
+
+// falsy value
+// false,0, -0, bigInt 0n, "", null, undefined, NaN
+
+
+// truthy value
+// "0", 1, 'false', " ", [], {}, function(){}
