c3a155510a19c3e6a7904c9a60020ef8a452596c
Sahil Sharma <78908493+Sahil1618@users.noreply.github.com>
2024-03-06 18:37:40 +0000

    Dom in js

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,8 +1,33 @@
 const myNums = [1, 2, 3, 4]
 
-const mytotal = myNums.reduce( function(acc, curval) {
-    console.log(`acc: ${acc} and curval ${curval}`);
-    return acc + curval 
-}, 0 )
+// const mytotal = myNums.reduce( function(acc, curval) {
+//     console.log(`acc: ${acc} and curval ${curval}`);
+//     return acc + curval 
+// }, 0 )
+
+const mytotal = myNums.reduce( (acc, curr) => acc + curr, 0 )
 
 console.log(mytotal);
+
+const shoppingcart = [
+    {
+        itemName: "Js course",
+        price: 2999
+    },
+    {
+        itemName: "Python",
+        price: 999
+    },
+    {
+        itemName: "Mobile Dev",
+        price: 5999
+    },
+    {
+        itemName: "Data Science",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingcart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
